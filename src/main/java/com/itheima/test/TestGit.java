25525a3896cc6f5cc242e02d4619f39d065c9744
root <1154587159@qq.com>
2018-12-30 08:46:07 +0800

    克隆后第二次修改

--- src/main/java/com/itheima/test/TestGit.java
+++ src/main/java/com/itheima/test/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("第一次写代码");
         System.out.println("克隆项目后第一次修改");
+        System.out.println("克隆项目后第二次修改");
     }
 }
